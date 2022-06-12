d7feb8ca04189f387d56b4f8ccfd99702b87810f
sisko81 <abdurcampbell@yahoo.com>
2022-06-12 09:19:46 +0900

    update

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,9 @@
 const repeatString = function(string, num) {
-  let repeatedStr = "";
-  for(let i = 0; i < num; i++) repeatedStr += string;
-  return repeatedStr
+ let repeatedStr = "";
+ for(i = 0; i < num; i++) {
+   repeatedStr += string;
+ }
+ return repeatedStr
 }
 
 // Do not edit below this line
