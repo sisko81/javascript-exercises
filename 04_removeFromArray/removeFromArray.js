4d0e0c65d05224b3198fc03c36490d37267d3624
sisko81 <abdurcampbell@yahoo.com>
2022-06-15 06:46:16 +0900

    Attempting to use the rest operator.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,5 @@
-const removeFromArray = function() {
- const array = [1,2,3,4];
- const alteredArray = array.splice(2,1);
- const newArray = alteredArray;
-console.log(array)
+const removeFromArray = function(...args) {
+ const array = args[0]
  return array
 };
 
