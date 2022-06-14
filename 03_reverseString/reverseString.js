71acaf8f08615e1f7d215a1bb05ed52877951aa7
sisko81 <abdurcampbell@yahoo.com>
2022-06-15 05:41:01 +0900

    update reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,9 @@
 const reverseString = function(str) {
-  const arrayStr = str.split("");
-  const reverseArray = arrayStr.reverse();
-  const joinArray = reverseArray.join("")
-
-  return joinArray
+  const originalArray = str.split('');
+  const reverseArray = originalArray.reverse();
+  const joinedArray = reverseArray.join('')
+  
+  return joinedArray
 
 };
 
