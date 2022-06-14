bde41861a29cb76b55d47c7f3ccfb479a9bbb0d7
sisko81 <abdurcampbell@yahoo.com>
2022-06-15 06:43:37 +0900

    update

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
 const removeFromArray = function() {
- 
+ const array = [1,2,3,4];
+ const alteredArray = array.splice(2,1);
+ const newArray = alteredArray;
+console.log(array)
+ return array
 };
 
 // Do not edit below this line
