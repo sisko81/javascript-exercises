9157ce71daf09b154d1536d36b14866ad3a946b6
sisko81 <abdurcampbell@yahoo.com>
2022-06-20 19:05:14 +0900

    Added sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,14 +1,14 @@
 const sumAll = function(min, max) {
-  if(!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
-  if (min < 0 || max < 0) return "ERROR"  
-  if (min > max) {
-      const t = min;
-      min = max;
-      max = t
-  }  
+  if(min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  if(!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
+  if(min < 0 || max < 0) return 'ERROR'
   let sum = 0;
   for(let i = min; i < max + 1; i++) {
-      sum += i
+    sum += i
   }
   return sum
 };
